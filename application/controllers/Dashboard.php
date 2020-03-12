406ea5540a49e01e72d2897734b5b52a05303802
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2020-03-12 11:17:24 +0700

    Membuat tampilan Dashboard

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller{
+  public function index(){
+    $this->load->view('v_dashboard');
+  }
+
+  public function admin(){
+    $this->load->view('v_dashboard_admin');
+  }
+}
