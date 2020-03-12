c0a9ea3bff98770bb4e45ef1b558311408deaa77
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2020-03-12 10:16:36 +0700

    Memperbaiki gitignore

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller{
   public function index(){
-    
+    $this->load->view('dashboard');
   }
 
   public function surat1(){
