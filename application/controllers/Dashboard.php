f2f4873de59184ea28199d9b83ea61834f27d9d4
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2020-03-14 00:35:02 +0700

    Membuat routing ke dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -9,4 +9,8 @@ class Dashboard extends CI_Controller{
   public function admin(){
     $this->load->view('v_dashboard_admin');
   }
+
+  public function operator(){
+    $this->load->view('v_dashboard_operator');
+  }
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -30,7 +30,7 @@
 
       <form action="../../index3.html" method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="text" class="form-control" placeholder="Username" name="username">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -38,7 +38,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" class="form-control" placeholder="Password" name="password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -56,7 +56,7 @@
           </div> -->
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            <button type="submit" class="btn btn-primary btn-block" name="masuk">Masuk</button>
           </div>
           <!-- /.col -->
         </div>
