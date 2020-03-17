d43ec92bb329e30bb45d854263733dfc5470c119
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2020-03-17 11:05:59 +0700

    revert

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -39,6 +39,7 @@
 <body class="hold-transition sidebar-mini layout-fixed">
   <div class="wrapper">
 
+<<<<<<< HEAD
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
@@ -54,6 +55,22 @@
    
       <li class="nav-item">
         
+=======
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light d-flex justify-content-between">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="index3.html" class="nav-link">Home</a>
+        </li>
+
+      </ul>
+      <li class="nav-item">
+        <a href="<?= base_url('auth/logout') ?>">logout</a>
+>>>>>>> parent of beefb8b... Menambahkan fungsi tambah operator untuk admin
       </li>
       </ul>
     </nav>
