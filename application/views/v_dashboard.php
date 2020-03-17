0ee5df9d21e1b5ec56ab5d7aa85675e669ca6830
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2020-03-17 11:14:05 +0700

    memperbaiki dashboard

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -1,10 +1,15 @@
+<?php
+$data = $this->session->get_userdata();
+//  var_dump($data); die;
+extract($data);
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SIDAAD</title>
+  <title>AdminLTE 3 | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
@@ -28,34 +33,10 @@
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<p class="ml-5">
-  <?php
-  $data = $this->session->get_userdata();
-  //  var_dump($data); die;
-  extract($data);
-  ?>
-</p>
 
 <body class="hold-transition sidebar-mini layout-fixed">
   <div class="wrapper">
 
-<<<<<<< HEAD
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="" class="nav-link">Home</a>
-      </li>
-      
-    </ul>
-   
-      <li class="nav-item">
-        
-=======
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light d-flex justify-content-between">
       <!-- Left navbar links -->
@@ -68,112 +49,111 @@
         </li>
 
       </ul>
-      <li class="nav-item">
-        <a href="<?= base_url('auth/logout') ?>">logout</a>
-<<<<<<< HEAD
->>>>>>> parent of beefb8b... Menambahkan fungsi tambah operator untuk admin
-=======
->>>>>>> parent of beefb8b... Menambahkan fungsi tambah operator untuk admin
+      <li class="nav-item" style="list-style: none">
+        <a href="<?= base_url('auth/logout') ?>" class="btn btn-danger">logout</a>
       </li>
       </ul>
     </nav>
     <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <!-- <span class="brand-text font-weight-light">Operator</span> -->
-    </a>
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="index3.html" class="brand-link d-flex align-items-center pl-2 pr-2 justify-content-between">
+        <i class="fas fa-file-signature    "></i>
+        <span class="brand-text font-font-weight-bolder">SIDAAD</span>
+      </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <div class="info">
-          <a href="#" class="d-block"><?= $username ?></a>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="info">
+            <a href="#" class="d-block"><?= $username ?> <span class="badge badge-primary">operator</span></a>
+          </div>
         </div>
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
-            <a href="<?=base_url('surat/index') ?>" class="nav-link active"> 
-              <i class="nav-icon fas fa-copy"></i>
-              <p>
-                Buat Surat
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="<?=base_url('surat/status') ?>" class="nav-link">
-                  <p>Surat Keterangan Status</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?=base_url('surat/usaha') ?>" class="nav-link">
-                  <p>Surat Keterangan Izin Usaha</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?=base_url('surat/skck') ?>" class="nav-link">
-                  <p>SKCK</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?=base_url('surat/keramaian') ?>" class="nav-link">
-                  <p>Surat Izin Keramaian</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?=base_url('surat/tidakmampu') ?>" class="nav-link">
-                  <p>Surat Keterangan Tidak Mampu</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?=base_url('surat/domisili') ?>" class="nav-link">
-                  <p>Surat Keterangan Domisili</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="<?=base_url('surat/penghasilan') ?>" class="nav-link">
-                  <p>Surat Keterangan Penghasilan</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-book"></i>
-              <p>
-                Log Surat
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./index.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Perbulan</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Log Suratku</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+            <li class="nav-item has-treeview menu-open">
+              <a href="#" class="nav-link active">
+                <i class="nav-icon fas fa-copy"></i>
+                <p>
+                  Buat Surat
+                  <i class="right fas fa-angle-left"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="./index.html" class="nav-link">
+                    <p>Surat Keterangan Status</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="./index2.html" class="nav-link">
+                    <p>Surat Keterangan Izin Usaha</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="./index3.html" class="nav-link">
+                    <p>SKCK</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="./index3.html" class="nav-link">
+                    <p>Surat Izin Keramaian</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="./index3.html" class="nav-link">
+                    <p>Surat Keterangan Tidak Mampu</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="./index3.html" class="nav-link">
+                    <p>Surat Keterangan Domisili</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="./index3.html" class="nav-link">
+                    <p>Surat Keterangan Penghasilan</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item has-treeview">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-book"></i>
+                <p>
+                  Log Surat
+                  <i class="right fas fa-angle-left"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="./index.html" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Perbulan</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="./index2.html" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Operator</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
+    </aside>
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
@@ -212,14 +192,13 @@
                   <i class="ion ion-stats-bars"></i>
                 </div>
               </div>
-              </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3>53</sup></h3>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-success">
+                <div class="inner">
+                  <h3>53<sup style="font-size: 20px">%</sup></h3>
 
                   <p>Data penduduk tersimpan</p>
                 </div>
@@ -277,11 +256,13 @@
   <script src="<?= base_url('assets/vendor/adminlte/') ?>plugins/moment/moment.min.js"></script>
   <script src="<?= base_url('assets/vendor/adminlte/') ?>plugins/daterangepicker/daterangepicker.js"></script>
   <!-- Tempusdominus Bootstrap 4 -->
-  <script src="<?= base_url('assets/vendor/adminlte/') ?>plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+  <script src="<?= base_url('assets/vendor/adminlte/') ?>plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js">
+  </script>
   <!-- Summernote -->
   <script src="<?= base_url('assets/vendor/adminlte/') ?>plugins/summernote/summernote-bs4.min.js"></script>
   <!-- overlayScrollbars -->
-  <script src="<?= base_url('assets/vendor/adminlte/') ?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <script src="<?= base_url('assets/vendor/adminlte/') ?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js">
+  </script>
   <!-- AdminLTE App -->
   <script src="<?= base_url('assets/vendor/adminlte/') ?>dist/js/adminlte.js"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
